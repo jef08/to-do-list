7a6b9d9536c32df3348e168c83477f7a7adc866d
jef08 <jasfielding@gmail.com>
2025-01-28 20:51:25 +0100

    Main landing page created, want to fix add button

--- webpack.config.js
+++ webpack.config.js
@@ -8,6 +8,7 @@ module.exports = {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
     clean: true,
+    publicPath: '/to-do-list/'
   },
   devtool: "eval-source-map",
   devServer: {
